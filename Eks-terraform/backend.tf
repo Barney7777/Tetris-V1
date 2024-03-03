b597c81739f30aef5def304599dd2705e8730f42
Barney Wang <wangyaxu7@gmail.com>
2024-03-03 19:43:50 +1100

    modified terraform file

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "ratmdatastore" # Replace with your actual S3 bucket name
-    key    = "Jenkins/terraform.tfstate"
+    bucket = "barney-terraform-remote-state" # Replace with your actual S3 bucket name
+    key    = "Jenkins/terraform/terraform.tfstate"
     region = "ap-southeast-2"
   }
 }
