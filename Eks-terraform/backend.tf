fe6f9141d81c72d76e61e1e3b1d949327b76f557
Barney7777 <122773145+Barney7777@users.noreply.github.com>
2024-04-13 22:08:24 +1000

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "barney-terraform-remote-state" # Replace with your actual S3 bucket name
+    bucket = "barney-github-terraform-state" # Replace with your actual S3 bucket name
     key    = "Githubacions/terraform/terraform.tfstate"
     region = "ap-southeast-2"
   }
